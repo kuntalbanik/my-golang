41c7a1242a903d4087376e27142ea475df211763
Kuntal Banik <deisiduke1986@gmail.com>
2025-10-29 08:50:30 +0530

    Slice modified

--- main.go
+++ main.go
@@ -5,24 +5,28 @@ import (
 	"mylearning/arraydata"
 	"mylearning/errorhandling"
 	"mylearning/function"
-	// "mylearning/myinput"
+	"mylearning/myinput"
+	"mylearning/slice"
 )
 
 func main() {
 	fmt.Println("Hello")
-	// myinput.CustomInput()
+	myinput.CustomInput()
 
 	var version = "1.0"
 	fmt.Println(version)
 
 	addition := function.Add(10, 20)
-	fmt.Println("Addition of two numbers :", addition)
+	fmt.Println("Addition of two numbers : ", addition)
 
 	division := function.Divide(30.0, 20.75)
-	fmt.Println("Division of two numbers :", division)
+	// Formatted output
+	fmt.Printf("Division of two numbers : %0.2f\n", division)
 
 	errorhandling.ErrorHandler()
 
 	arraydata.Array()
+
+	slice.SliceCustom()
 
 }
--- slice/slice.go
+++ slice/slice.go
@@ -3,6 +3,16 @@ package slice
 import "fmt"
 
 func SliceCustom() {
+
+	num_slice := []int{1, 2, 3, 4, 5}
+	fmt.Println(num_slice)
+
 	numbers := make([]int, 3, 5)
+	numbers = append(numbers, 5, 6, 3, 445, 6)
 	fmt.Println(numbers)
+
+	out_len := len(numbers)
+	fmt.Println("Slice length", out_len)
+	out_cap := cap(numbers)
+	fmt.Println("Slice capacity", out_cap)
 }
